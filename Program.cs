06c122243a1a4d8d55963913d1393b71a8a86b93
alanhmr01 <alan.hmr@gmail.com>
2023-04-02 09:17:20 -0300

    Completo

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Exerciciolista;
+
+List<Vendas> vendas = new List<Vendas>();
+
+for (int i = 1; i <= 5; i++)
+{
+    Vendas venda = new Vendas();
+    venda.IdVenda = i;
+    venda.DataVenda = DateTime.Now.AddDays(-i);
+    venda.Produtos = new List<string>() { "Produto 1", "Produto 2", "Produto 3", "Produto 4", "Produto 5", "Produto 6", "Produto 7", "Produto 8", "Produto 9", "Produto 10" };
+    vendas.Add(venda);
+}
+
+foreach (Vendas venda in vendas)
+{
+    venda.ImprimirLista();
+}
---
+++ Vendas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerciciolista
+{
+    public class Vendas
+    {
+        public int IdVenda { get; set; }
+        public DateTime DataVenda { get; set; }
+        public List<string> Produtos { get; set; }
+
+        public void ImprimirLista()
+        {
+            Console.WriteLine("ID da Venda: " + IdVenda);
+            Console.WriteLine("Data da Venda: " + DataVenda);
+            Console.WriteLine("Produtos Comprados:");
+            foreach (string produto in Produtos)
+            {
+                Console.WriteLine("- " + produto);
+            }
+            Console.WriteLine();
+        }
+    }
+}
