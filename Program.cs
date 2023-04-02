61cb5b0d44c551e2bb3a219abc06c391d6cef470
Alan Rocha <alan.hmr@gmail.com>
2023-04-02 09:27:51 -0300

    Correcçoes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ for (int i = 1; i <= 5; i++)
     Vendas venda = new Vendas();
     venda.IdVenda = i;
     venda.DataVenda = DateTime.Now.AddDays(-i);
-    venda.Produtos = new List<string>() { "Produto 1", "Produto 2", "Produto 3", "Produto 4", "Produto 5", "Produto 6", "Produto 7", "Produto 8", "Produto 9", "Produto 10" };
+    venda.Produtos = new List<string>() { "Tulipa", "Gelo", "Cerveja", "Carvão", "Pinga", "Limão", "Picanha", "Linguiça", "Pão de Alho", "Queijo Coalho" };
     vendas.Add(venda);
 }
 
